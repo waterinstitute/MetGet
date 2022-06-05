4daa5bb305558a2129b4ed0a1625d37e5238fbc9
Zach Cobell <zcobell@gmail.com>
2022-06-05 04:27:14 +0000

    Generalizing COAMPS/HWRF interface

--- python_functions/metbuild/metbuild/database.py
+++ python_functions/metbuild/metbuild/database.py
@@ -38,9 +38,9 @@ class Database:
         elif service == "nam-ncep":
             return self.generate_generic_file_list("nam_ncep", param, start, end, nowcast, multiple_forecasts)
         elif service == "hwrf":
-            return self.generate_hwrf_file_list(start, end, storm, nowcast, multiple_forecasts)
+            return self.generate_storm_file_list("hwrf", start, end, storm, nowcast, multiple_forecasts)
         elif service == "coamps-tc":
-            return self.generate_coamps_file_list(start, end, storm, nowcast, multiple_forecasts)
+            return self.generate_storm_file_list("coamps_tc", start, end, storm, nowcast, multiple_forecasts)
         else:
             print("ERROR: Invalid data type")
             sys.exit(1)
@@ -126,42 +126,19 @@ class Database:
             return_list.append([f[2], f[3]])
         return return_list
 
-
-    def generate_hwrf_file_list(self, start, end, storm, nowcast, multiple_forecasts):
-
-       # ... Generate some selections into a temporary table. This essentially duplicates the gfs style database
-        sql_tmptable = "create temporary table tmptbl1 select id,forecastcycle,forecasttime,filepath from hwrf where stormname = '"+storm+"';"
-        sql_tmptable2 = "create temporary table tmptbl2 select * from tmptbl1;"
-        sql = "select t1.id,t1.forecastcycle,t1.forecasttime,t1.filepath from tmptbl1 " \
-             " t1 JOIN(select forecasttime, max(id) id FROM tmptbl2 group by forecasttime order by forecasttime) t2 " \
-             "ON t1.id = t2.id AND t1.forecasttime = t2.forecasttime AND t1.forecasttime >= '" + start.strftime(
-             "%Y-%m-%d %H:%M:%S") + "' AND t1.forecasttime <= '" + end.strftime(
-             "%Y-%m-%d %H:%M:%S") + "';"
-
-        self.cursor().execute(sql_tmptable)
-        self.cursor().execute(sql_tmptable2)
-        self.cursor().execute(sql)
-        rows = self.cursor().fetchall()
-        return_list = []
-        for f in rows:
-            return_list.append([f[2], f[3]])
-        self.cursor().execute("drop temporary table tmptbl1;")
-        self.cursor().execute("drop temporary table tmptbl2;")
-        return return_list
-    
-    def generate_coamps_file_list(self, start, end, storm, nowcast, multiple_forecasts):
+    def generate_storm_file_list(self, data_type, start, end, storm, nowcast, multiple_forecasts):
         from datetime import timedelta
         if nowcast:
-            return self.generate_coamps_file_list_nowcast(start, end, storm)
+            return self.generate_storm_file_list_nowcast(data_type, start, end, storm)
         else:
             if multiple_forecasts:
-                return self.generate_coamps_file_list_multiple_forecasts(start, end, storm)
+                return self.generate_storm_file_list_multiple_forecasts(data_type, start, end, storm)
             else:
-                return self.generate_coamps_file_list_single_forecast(start, end, storm)
+                return self.generate_storm_file_list_single_forecast(data_type, start, end, storm)
     
-    def generate_coamps_file_list_nowcast(self, start, end, storm):
-       # ... Generate some selections into a temporary table. This essentially duplicates the gfs style database
-        sql_tmptable = "create temporary table tmptbl1 select id,forecastcycle,forecasttime,filepath from coamps_tc where stormname = '"+storm+"';"
+    def generate_storm_file_list_nowcast(self, data_type, start, end, storm):
+        # ... Generate some selections into a temporary table. This essentially duplicates the gfs style database
+        sql_tmptable = "create temporary table tmptbl1 select id,forecastcycle,forecasttime,filepath from "+data_type+" where stormname = '"+storm+"';"
         sql_tmptable2 = "create temporary table tmptbl2 select * from tmptbl1;"
         sql = "select t1.id,t1.forecastcycle,t1.forecasttime,t1.filepath from tmptbl1" \
              " t1 JOIN(select forecasttime, max(id) id FROM tmptbl2 group by forecasttime order by forecasttime) t2 " \
@@ -179,9 +156,9 @@ class Database:
         self.cursor().execute("drop temporary table tmptbl2;")
         return return_list
     
-    def generate_coamps_file_list_single_forecast(self, start, end, storm):
-       # ... Generate some selections into a temporary table. This essentially duplicates the gfs style database
-        sql_tmptable = "create temporary table tmptbl1 select id,forecastcycle,forecasttime,filepath from coamps_tc where stormname = '"+storm+"';"
+    def generate_storm_file_list_single_forecast(self, data_type, start, end, storm):
+        # ... Generate some selections into a temporary table. This essentially duplicates the gfs style database
+        sql_tmptable = "create temporary table tmptbl1 select id,forecastcycle,forecasttime,filepath from "+data_type+" where stormname = '"+storm+"';"
         sql_tmptable2 = "create temporary table tmptbl2 select * from tmptbl1;"
         sql = "select t1.id,t1.forecastcycle,t1.forecasttime,t1.filepath from tmptbl1" \
               " t1 JOIN(select forecasttime, max(id) id FROM tmptbl2 group by forecasttime order by forecasttime) t2 " \
@@ -208,9 +185,9 @@ class Database:
         self.cursor().execute("drop temporary table tmptbl2;")
         return return_list
     
-    def generate_coamps_file_list_multiple_forecasts(self, start, end, storm):
+    def generate_storm_file_list_multiple_forecasts(self, data_type, start, end, storm):
        # ... Generate some selections into a temporary table. This essentially duplicates the gfs style database
-        sql_tmptable = "create temporary table tmptbl1 select id,forecastcycle,forecasttime,filepath from coamps_tc where stormname = '"+storm+"';"
+        sql_tmptable = "create temporary table tmptbl1 select id,forecastcycle,forecasttime,filepath from "+data_type+" where stormname = '"+storm+"';"
         sql_tmptable2 = "create temporary table tmptbl2 select * from tmptbl1;"
         sql = "select t1.id,t1.forecastcycle,t1.forecasttime,t1.filepath from tmptbl1" \
               " t1 JOIN(select forecasttime, max(id) id FROM tmptbl2 group by forecasttime order by forecasttime) t2 " \
