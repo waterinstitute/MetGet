e6bb94f6fcb23ad2754e0274070f01f9962f7465
Zach Cobell <zcobell@gmail.com>
2023-04-05 08:37:55 -0600

    Upadting rabbitmq to single yaml

--- k8s/argo/patch_argo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-kubectl patch deployment \
-  argo-server \
-  --namespace metget \
-  --type='json' \
-  -p='[{"op": "replace", "path": "/spec/template/spec/containers/0/args", "value": ["server","--auth-mode=server"]}]'
