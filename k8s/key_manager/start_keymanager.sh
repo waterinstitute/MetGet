12b9745c061d0b9ac3beae332adeb8ed78515cef
Zach Cobell <zcobell@gmail.com>
2023-05-04 18:47:41 -0600

    Adding a key manager

---
+++ k8s/key_manager/start_keymanager.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+kubectl -n metget exec -it metget-keymanager -- /bin/bash
--- libraries/metbuild/metbuild/database.py
+++ libraries/metbuild/metbuild/database.py
@@ -1,34 +1,53 @@
-
-
 def __init_database():
+    """
+    Initialize the database engine and return it.
+    """
     import os
     from sqlalchemy import create_engine, URL
-    dbhost = os.environ["METGET_DATABASE_SERVICE_HOST"]
-    dbpassword = os.environ["METGET_DATABASE_PASSWORD"]
-    dbusername = os.environ["METGET_DATABASE_USER"]
-    dbname = os.environ["METGET_DATABASE"]
+
+    db_host = os.environ["METGET_DATABASE_SERVICE_HOST"]
+    db_password = os.environ["METGET_DATABASE_PASSWORD"]
+    db_username = os.environ["METGET_DATABASE_USER"]
+    db_name = os.environ["METGET_DATABASE"]
     url = URL.create(
         "postgresql+psycopg2",
-        username=dbusername,
-        password=dbpassword,
-        host=dbhost,
-        database=dbname,
+        username=db_username,
+        password=db_password,
+        host=db_host,
+        database=db_name,
     )
     return create_engine(
         url, isolation_level="REPEATABLE_READ", pool_pre_ping=True, pool_size=20
     )
 
+
+# Initialize the database engine and store it in a global variable.
 DATABASE_ENGINE = __init_database()
 
+
 class Database:
+    """
+    A class to manage the database connection.
+    """
+
     def __init__(self):
+        """
+        Initialize the database session
+        """
         self.__session = None
         self.__connect()
 
     def __connect(self):
+        """
+        Connect to the database and generate a session.
+        """
         from sqlalchemy.orm import sessionmaker
+
         Session = sessionmaker(bind=DATABASE_ENGINE)
         self.__session = Session()
 
     def session(self):
+        """
+        Return the database session.
+        """
         return self.__session
